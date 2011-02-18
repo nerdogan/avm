0a05ae19a8688bcb5a8232ace9a8c269f2ac84fe
Ondřej Brejla <ondrej@brejla.cz>
2011-02-18 12:36:48 +0100

    NetteGoToViewAction: init

---
+++ src/org/netbeans/modules/php/nette/actions/NetteGoToViewAction.java
@@ -0,0 +1,65 @@
+/*
+ *  The MIT License
+ * 
+ *  Copyright (c) 2011 Radek Ježdík <redhead@email.cz>, Ondřej Brejla <ondrej@brejla.cz>
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ * 
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+package org.netbeans.modules.php.nette.actions;
+
+import org.netbeans.modules.csl.api.UiUtils;
+import org.netbeans.modules.php.api.editor.EditorSupport;
+import org.netbeans.modules.php.api.editor.PhpBaseElement;
+import org.netbeans.modules.php.nette.utils.EditorUtils;
+import org.netbeans.modules.php.spi.actions.GoToViewAction;
+import org.openide.filesystems.FileObject;
+import org.openide.util.Lookup;
+
+/**
+ *
+ * @author Ondřej Brejla <ondrej@brejla.cz>
+ */
+public class NetteGoToViewAction extends GoToViewAction {
+
+	private FileObject fo;
+
+	private int offset;
+
+	public NetteGoToViewAction(FileObject fo, int offset) {
+		this.fo = fo;
+        this.offset = offset;
+	}
+
+	@Override
+	public void actionPerformedInternal() {
+		EditorSupport editorSupport = Lookup.getDefault().lookup(EditorSupport.class);
+        PhpBaseElement phpElement = editorSupport.getElement(fo, offset);
+		if (phpElement == null) {
+            //return false;
+        }
+        FileObject view = EditorUtils.getView(fo, phpElement);
+        if (view != null) {
+            UiUtils.open(view, DEFAULT_OFFSET);
+            //return true;
+        }
+        //return false;
+	}
+
+}
