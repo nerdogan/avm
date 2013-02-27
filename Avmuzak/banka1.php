f315eb6967ef97e1afab4f3d708194c171c5723a
NAMIK ERDOĞAN <erdogannamik@gmail.com>
2013-02-27 02:26:52 +0200

    1.45 Banka Bölümü Hareket insert eklendi. Bazı hatalar giderildi.

--- Avmuzak/banka1.php
+++ Avmuzak/banka1.php
@@ -33,9 +33,9 @@ function hesapla(id1, id2, id3) {
     <li><a class="btn btn-warning" href="#"><i class="cus-application"></i> Banka Bölümü</a></li>
     <br><br>
 <?php if( protectThis(1) ) : ?>
-<li><a class="btn btn-large btn-success" href="banka.php?do=yeni"><?php _e('Yeni Kayıt'); ?></a></li><br>
+<li><a class="btn btn-large btn-success" href="banka1.php?do=yeni"><?php _e('Yeni Kayıt'); ?></a></li><br>
 <li><a class="btn btn-large btn-danger" href="#"><?php _e('Sil İptal'); ?></a></li><br>
-<li><a class="btn btn-large btn-info" href="banka.php?do=liste"><?php _e('Tam Liste'); ?></a></li><br>
+<li><a class="btn btn-large btn-info" href="banka1.php?do=liste"><?php _e('Tam Liste'); ?></a></li><br>
 <li><a href="#"><?php _e('');   ?></a></li>
 </ul>
 </div>
@@ -59,26 +59,22 @@ function hesapla(id1, id2, id3) {
 <?php 
 
 $id = $_POST['id'];
-$bankaadi = $_POST['bankaadi'];
-$hesapsahibi = $_POST['hesapsahibi'];
-$subeadi = $_POST['subeadi'];
-$hesapno = $_POST['hesapno'];
-$parabirimi = $_POST['parabirimi'];
-$telefon1 = $_POST['telefon1'];
-$telefon2 = $_POST['telefon2'];
-$faks = $_POST['faks'];
-$notlar = $_POST['notlar'];
-$hesapbakiye = $_POST['hesapbakiye'];
-$iban = $_POST['iban'];
-$email = $_POST['email'];
-$param = array (':id'=>$id,':bankaadi'=>$bankaadi,':hesapsahibi'=>$hesapsahibi,':subeadi'=>$subeadi,':hesapno'=>$hesapno,':parabirimi'=>$parabirimi,':telefon1'=>$telefon1,':telefon2'=>$telefon2,':faks'=>$faks,':notlar'=>$notlar,':hesapbakiye'=>$hesapbakiye,':iban'=>$iban,':email'=>$email);
-if (!empty($bankaadi)){
-$query11 = "INSERT INTO bankakart (id,bankaadi,hesapsahibi,subeadi,hesapno,parabirimi,telefon1,telefon2,faks,notlar,hesapbakiye,iban,email) VALUES ( :id,:bankaadi,:hesapsahibi,:subeadi,:hesapno,:parabirimi,:telefon1,:telefon2,:faks,:notlar,:hesapbakiye,:iban,:email )";
+$banka_id = $_POST['banka_id'];
+$parts = explode('.', $_POST['tarih']);
+$tarih  = "$parts[2]-$parts[1]-$parts[0]";
+$dekontno = $_POST['dekontno'];
+$tutar = $_POST['tutar'];
+$aciklama = $_POST['aciklama'];
+$harekettipi = $_POST['harekettipi'];
+$op = $_POST['op'];
+$kullanici_no_ekleyen = $_POST['kullanici_no_ekleyen'];
+$kullanici_no_duzenleyen = $_POST['kullanici_no_duzenleyen'];
+$param = array (':id'=>$id,':banka_id'=>$banka_id,':tarih'=>$tarih,':dekontno'=>$dekontno,':tutar'=>$tutar,':aciklama'=>$aciklama,':harekettipi'=>$harekettipi,':op'=>$op,':kullanici_no_ekleyen'=>$kullanici_no_ekleyen,':kullanici_no_duzenleyen'=>$kullanici_no_duzenleyen);
+if (!empty($banka_id)){
+$query11 = "INSERT INTO banka_hareket (id,banka_id,tarih,dekontno,tutar,aciklama,harekettipi,op,kullanici_no_ekleyen,kullanici_no_duzenleyen) VALUES ( :id,:banka_id,:tarih,:dekontno,:tutar,:aciklama,:harekettipi,:op,:kullanici_no_ekleyen,:kullanici_no_duzenleyen )";
 $result = $generic->query($query11, $param);
- if ($result->rowCount()==1): echo"kaydedildi";  header( 'Location: banka.php?do=liste&msg=1' ) ; endif;
+ if ($result->rowCount()==1): echo"kaydedildi";  header( "Location: banka1.php?do=liste&msg=1" ) ; endif;
 }
-
- 
  
  
 
@@ -86,16 +82,22 @@ $result = $generic->query($query11, $param);
 
 if(($_GET['do'] === "liste")  ): 
 echo "<table>";
-    echo "<tr><td>Adı </td><td>Şube</td><td>Hesap No</td><td style='width:150px'>Bakiye</td><td>&nbsp;&nbsp;&nbsp;</td><td>Açıklama</td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>";
+    echo "<tr><td>Tarih </td><td>Hareket Tipi</td><td>Tutar</td><td >Dekont No</td><td style='width:300px'>Açıklama&nbsp;</td><td></td><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>";
      $number=0;  
-     $kackayit=$generic->query('SELECT *  FROM bankakart ');
+     $kackayit=$generic->query('SELECT *  FROM banka_hareket ORDER BY tarih asc');
      $toplam=0.0;
 foreach($kackayit as $row) {
     $number++;
-    echo "<tr class='",( ($number & 1) ? 'success' : 'info' ),"'><td> ",$row[1],"</td><td> ",$row[3],"</td><td> ",$row[4],"</td><td> ",$row[10],"</td><td> ",$row[5],"</td><td> ",$row[9],"</td><td><a href='banka.php?do=duzenle&id=",$row['id'],"'>Düzenle</a></td></tr>" ;
-    $toplam+=$row[10];
+    echo "<tr class='",( ($number & 1) ? 'success' : 'info' ),"'><td> ",$row[2],"</td><td> ",$row[6],"</td><td> ",$row[4],"</td><td> ",$row[3],"</td><td> ",$row[5],"</td><td> ",$row[7],"</td><td><a href='banka.php?do=duzenle&id=",$row['id'],"'>Düzenle</a></td></tr>" ;
+    if ($row[6]=="1"):{
+        $toplam+=$row[4];
     }
- echo "<tr class='",( ($number & 1) ? 'success' : 'info' ),"'><td> </td><td> </td><td></td><td> </td><td></td><td></td><td></td></tr>" ;    
+        else: {
+    $toplam-=$row[4];
+}
+endif;
+    }
+ echo "<tr class='",( ($number & 1) ? 'success' : 'info' ),"'><td></td><td>Bakiye : </td><td>",$toplam,"<td> </td><td> </td></td><td></td><td></td></tr>" ;    
 echo '</table';  
 
     
@@ -160,7 +162,7 @@ if(($_GET['do'] === "yeni")  ): ?>
 <B>&nbsp;tutar</B>
 </span></div></TD>
 <TD class='row4'> <div align='left'><span class='postdetails'>
-<input type = 'text' name = 'tutar' size = '5+5' maxlength = '' value = "<? echo $tutar; ?>">
+<input type = 'text' name = 'tutar'  value = "<? echo $tutar; ?>">
 </span></div></TD>
 </TR>
 <TR>
@@ -175,8 +177,13 @@ if(($_GET['do'] === "yeni")  ): ?>
 <TD class='row4'> <div align='left'><span class='postdetails'>
 <B>&nbsp;harekettipi</B>
 </span></div></TD>
-<TD class='row4'> <div align='left'><span class='postdetails'>
-<input type ='text' name = 'harekettipi' size = '55' maxlength = '50' value = "<? echo $harekettipi; ?>">
+<TD class='row4' > <div align='left'><span class='postdetails'>
+         <select class="form-dropdown" style="width:200px" name = 'harekettipi' >
+            <option>  </option>
+           <option value="1" selected> Yatırılan Gelen (+) </option>
+            <option value="2" selected> Çekilen Giden (-) </option>
+          </select>
+
 </span></div></TD>
 </TR>
 <TR>
